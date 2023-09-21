cdeac5f2c32cd84d9542664cdbd21c21a50479a5
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2023-09-20 22:13:49 -0300

    some messages changes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    @Get()
-    listMessages() {
+
+    constructor(public messagesService: MessagesService){
 
     }
+
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+
     @Post()
-    createMessage() { }
+    createMessage(@Body() body: CreateMessageDto) { 
+        return this.messagesService.create(body.content)
+    }
 
     @Get('/:id')
-    getMessage() { }
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
+
+        if(!message){
+            throw new NotFoundException("message not found");
+        }
+
+        return message;
+     }
 }
